c26cdbb79aa837ef52de74900ea5d6ac4c4dd586
Aniket Kumar <aniketcodes7@gmail.com>
2023-08-28 15:35:35 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Footer() {
+  let date = new Date();
+
+  let year = date.getFullYear();
+
+  return (
+    <footer>
+      <p>copyright {year} </p>
+    </footer>
+  );
+}
+
+export default Footer;
